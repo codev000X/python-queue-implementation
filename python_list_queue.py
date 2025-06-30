f303fc33d8a1080f13138b9223bf3f0e39dcc526
Shahzaib <heneryjeans1101@gmail.com>
2025-06-30 10:23:12 +0500

    add the fixes

--- python_list_queue.py
+++ python_list_queue.py
@@ -5,3 +5,18 @@ class Queue:
     def __str__(self):
         values = [str(x) for x in self.item]
         return " ".join(values)
+    
+    def isEmpty(self):
+        if self.item == []:
+            return True
+        return False
+    
+    def enqueue(self , value):
+        self.item.append(value)
+        return 'Successfully addded .'
+    
+    def dequeue(self):
+        if self.isEmpty():
+            return 'Queue is Empty .'
+        self.item.pop(0)
+        
