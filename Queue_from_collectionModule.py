d8e98a80283f95b0f5cb0e0a73c32f12e02b4188
Shahzaib <heneryjeans1101@gmail.com>
2025-07-01 12:43:15 +0500

    file : add file for creationof queue from collections module .

---
+++ Queue_from_collectionModule.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+# Creating the Queue.
+customqueue = deque(maxlen = 3)
+print(customqueue)
+
+# Appending to Queue .
+customqueue.append(1)
+customqueue.append(2)
+customqueue.append(3)
+print(customqueue)
+
+# If the length reaches to max then append method DoNot raise error but it overwrites the first element .
+customqueue.append(4)
+print(customqueue)
+
+# Dequeuing via popleft() .
+customqueue.popleft()
+print(customqueue)
